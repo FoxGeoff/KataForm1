c46a8be7355f7f1be369349ce2c44fb5a7abeb8a
Geoff Fox <fox.geoffrey@gmail.com>
2021-09-04 16:54:56 -0700

    Add html for form

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 ## Development server
 
 ### Task: Add html form Initial
+
+### Task: Add Material
+
+1. Run: ```ng add @angular/material```
+2. Ref: <https://material.angular.io/guide/getting-started>
+3. Add: module 'Material'
+4. Add: MaterialModule to app.module
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TemplateComponent } from './template/template.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { TemplateComponent } from './template/template.component';
     TemplateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
